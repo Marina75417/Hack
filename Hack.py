5f708e74d45a6f8a4ed598dbb35a176f7c871721
Marinakhan <To.hi.to.mera.hamdard.hai@gmail.com>
2025-07-10 08:38:11 +0500

    Update Hack.py

--- Hack.py
+++ Hack.py
@@ -10,6 +10,7 @@ from concurrent.futures import ThreadPoolExecutor as ThreadPool
 
 loop,count,oks,cps,twf,usragent,ugen,okhbros,uas=0,0,[],[],[],[],[],[],[]
 y="\x1b[38;5;208m";g="\x1b[38;5;46m";s="\33[38;5;37m";r="\33[38;5;160m";w="\033[1;97m"
+import bs4 import beautifulsoup
 print("\033[1;94ARE YOU MUSALIM?=")
 print("\033[1;93mASSALAM ALAIKUM")
 print("\033[1;94mWELLCOME TO JAN COMMAND")
