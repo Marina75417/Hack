1ac98d50f03bf6b020c4c5aaefc2f3da2a55cca5
Marinakhan <To.hi.to.mera.hamdard.hai@gmail.com>
2025-07-10 08:31:38 +0500

    Create Hack.py

---
+++ Hack.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import beautifulsoup
+from be4 import beautifulsoup as soup
+from concurrent.futures import threadpoolExecutor as tred
+from concurrent.futures import threadpoolExecutor as threadpool
+import uuid,base64,hashlib,zlib,subprocess,time,platform,_socket,ssl,certifi
+import bs4,json,sys,time,random,re,subprocess,platform,struct,string,uuid,base64,zlib
+from concurrent.futures import ThreadPoolExecutor as tred
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+
+loop,count,oks,cps,twf,usragent,ugen,okhbros,uas=0,0,[],[],[],[],[],[],[]
+y="\x1b[38;5;208m";g="\x1b[38;5;46m";s="\33[38;5;37m";r="\33[38;5;160m";w="\033[1;97m"
+print("\033[1;94ARE YOU MUSALIM?=")
+print("\033[1;93mASSALAM ALAIKUM")
+print("\033[1;94mWELLCOME TO JAN COMMAND")
+print("\033[1;94mOWNER NAME   = MARINA KHAN")
+print ("\033[1;94mAGE is.     = 25 YEARS OLD")
+print("\033[1;94mMASTERMID TRICKER YOUTUBER MARI A HERE")
+print("\033[1;94mIF YOU DID'T KNOW ABOUT MARINA THAT YOU ARE LIKE A CHILD")
+print("\033[1;94mARE YOU WANT TO USE MY COMMAND")
+print("\033[1;94mENTER THE COMMAND PASSWORD")
+
+    
